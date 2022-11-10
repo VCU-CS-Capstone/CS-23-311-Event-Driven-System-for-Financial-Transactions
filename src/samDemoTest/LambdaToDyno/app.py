5d1de5f2019625530d2e06d07fa4be6393360c16
Dan Tran <dan2001tran@gmail.com>
2022-11-10 17:19:22 -0500

    adding permissions to the yaml

--- src/samDemoTest/LambdaToDyno/app.py
+++ src/samDemoTest/LambdaToDyno/app.py
@@ -2,8 +2,59 @@ import json
 import boto3
 import requests
 
-def lambda_handler(event,context):
-    dynamodb = boto3.resource('dynamodb')
-    table = dynamodb.Table('MyDynamoDBTable')
-    input = event['Records'][0]['body']
-    response = table.put_item(Item = input)
+def updateDynamo(Item):
+    client = boto3.resource('dynamodb')
+    table = client.Table('Transactions')
+    response = table.put_item(Item = Item)
+    
+def lambda_handler(event, context):
+    
+#     account['id'] = str(uuid.uuid4())
+#   account['object'] = 'customer'
+#   account["address"] = address
+#   account["balance"] = '0'
+#   account["created"] = str(date.today())
+#   account["defaultSource"] = ' '
+#   account['delinquent'] = 'true'
+#   account['description'] = name
+#   account['email'] = email
+#   account['name'] = name
+#   account['phone'] = phone
+#   account['shipping'] = address
+    print(event)
+    parsed = json.loads(event['Records'][0]['body'])
+    print(parsed)
+    id = parsed['detail']['id']
+    object = parsed['detail']['object']
+    address = event['detail']['address']
+    balance = event['detail']['balance']
+    created = event['detail']['created']
+    defaultSource = event['detail']['defaultSource']
+    delinquent = event['detail']['delinquent']
+    description = event['detail']['description']
+    email = event['detail']['email']
+    name = event['detail']['name']
+    phone = event['detail']['phone']
+    shipping = event['detail']['shipping']
+    
+    Item = {
+        'id' : id,
+        'object' : object,
+        'address' : address,
+        'balance' : balance,
+        'created' : created,
+        'defaultSource' : defaultSource,
+        'delinquent' : delinquent,
+        'description' : description,
+        'email' : email,
+        'name' : name,
+        'phone' : phone,
+        'shipping' : shipping
+    }
+    response = updateDynamo(Item)
+    print(Item)
+    print (Item)
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Db added!')
+    }
