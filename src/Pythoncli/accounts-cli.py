241534686b265110fe58c18dc1a5c49635e0ca9e
Dan Tran <dan2001tran@gmail.com>
2023-02-08 14:12:46 -0500

    adressing last code review

--- src/Pythoncli/accounts-cli.py
+++ src/Pythoncli/accounts-cli.py
@@ -1,4 +1,5 @@
 import os
+import boto3
 from stat import SF_SNAPSHOT
 import sys
 import json
@@ -15,11 +16,21 @@ transaction = {}
 client = boto3.client('events')
 
 def CreateAccount():
-<<<<<<< HEAD
-  account = {}
-=======
->>>>>>> a96fdd7f73400930dd8cbc762cb676395aa8c917
   cardType = input("What card would you like to choose? \n (1) American Dragon Card \n (2) Big Baller Card \n")
+  notValid = True
+  if cardType == '1':
+    cardTypeString = 'American Dragon Card'
+  elif cardType == '2':
+    cardTypeString = 'Big Baller Card' 
+  else:
+    while notValid:
+      userChoice = input("Invalid choice! Please only enter 1 or 2 \n")
+      if userChoice == '1':
+        cardTypeString = 'American Dragon Card'
+        notValid = False
+      elif userChoice == '2':
+        cardTypeString = 'Big Baller Card' 
+        notValid = False
   cardNum = input("Whats your card id \n")
   name = input("What is your name? \n")
   ssn = input("What is you social security? \n")
@@ -27,47 +38,27 @@ def CreateAccount():
   address = input("What is your shipping address? \n")
   email = input("What is your email? \n")
   phone = input("What is your phone number?\n")
-<<<<<<< HEAD
-  cardTypeString = ""
-  if cardType == 1:
-    cardTypeString = 'American Dragon Card'
-  elif cardType == 2:
-    cardTypeString == 'Big Baller Card'
-  account['id'] = str(uuid.uuid4())
-=======
-  if cardType == '1':
-    cardTypeString = 'American Dragon Card'
-  elif cardType == '2':
-    cardTypeString = 'Big Baller Card' #Error handling for other card types? 
-  # account['id'] = str(uuid.uuid4()) #Use an expanded form for setting the account variable (see below)
-  account['id'] = '4444' #Can this not be hardcoded? Use .env or maybe lookup the account in Dyanmo?
->>>>>>> a96fdd7f73400930dd8cbc762cb676395aa8c917
-  account['creditCardId'] = cardNum
-  account['name'] = name
-  account['address'] = address
-  account['ssn'] = ssn
-  account['birthdate'] = dob
-  account['email'] = email
-  account['phone'] = phone
-  account['cardType'] = cardTypeString
-  account['currentBalance'] = 0
-  account['previousBalance'] = 0
-  account['Type'] = "Book New Account"
-<<<<<<< HEAD
-  if cardType == 1:
-    account['maxCredit'] = 400
-  elif cardType == 2:
-    account['maxCredit'] = 100000
- 
-  accountUpdate = json.dumps(account)
-  return accountUpdate
-=======
+
+  account = {
+  'id' : '4444', #Can this not be hardcoded? Use .env or maybe lookup the account in Dyanmo?
+  'creditCardId' : cardNum,
+  'name' :name,
+  'address' : address,
+  'ssn' : ssn,
+  'birthdate' : dob,
+  'email' : email,
+  'phone' : phone,
+  'cardType' : cardTypeString,
+  'currentBalance' : 0,
+  'previousBalance' : 0,
+  'Type' : "Book New Account"
+  }
   if cardType == '1':
     account['maxCredit'] = 400
   elif cardType == '2':
     account['maxCredit'] = 100000
   
-  print(account) #Can add statement before print print("account:", account)
+  print("account" , account) 
   response = client.put_events(
         Entries=[
             {
@@ -118,26 +109,24 @@ def CreateNewTransaction():
                 ]
             )
   print(response)
-  time.sleep(15) #Why is sleep needed here? 
   Authorization(transaction['AccountId'], transaction['CreditCardId'], transaction['Price'], transaction)
 
 def Authorization(AccountId, creditCardId, costOfTransaction, transaction):
-  client2 = boto3.resource('dynamodb') #Use semantic naming, not client2 so you know what it is
-  table = client2.Table('AccountDB') #same as above
-  response = table.get_item(Key={'AccountId': AccountId, 'creditCardId' : creditCardId })
+  DynamoDB = boto3.resource('dynamodb') 
+  AcountDbTable = DynamoDB.Table('AccountDB') 
+  response = AcountDbTable.get_item(Key={'AccountId': AccountId, 'creditCardId' : creditCardId })
   entry=response['Item']
-  currentBalance = float(entry['currentBalance']) #What are these values if you don't cast them as float?
+  currentBalance = float(entry['currentBalance']) 
   maxCredit = float(entry['maxCredit'])
   cost = float(transaction['Price'])
-  if currentBalance + cost< maxCredit: #What if it is equal?
+  if currentBalance + cost <= maxCredit: 
     MerchentFunded = input("Press a key to fund merchant\n")
     fund(transaction)
   else:
     print("Insufficent Balance")
 
 def fund(transaction):
-  print('price is')#Combine print statements
-  print(transaction['Price'])
+  print('price is', transaction['Price'])
   client4 = boto3.client('events')
   Item = { #This sytax is good!
     'TransactionId' : transaction['TransactionId'],
@@ -169,29 +158,18 @@ def fund(transaction):
 
 
 choice = input("What would you like to do \n (1) Create a new Account \n (2) Make a new Transaction \n")
-
+notValid = True
 if choice == '1':
   CreateAccount()
-if choice == '2': #Use elif, plus else
+elif choice == '2': 
   CreateNewTransaction()
+else:
+  while notValid:
+    choice = input("Incorrect choice! Only enter either 1 or 2 \n")
+    if choice == '1':
+      CreateAccount()
+      notValid = False
+    elif choice == '2': 
+      CreateNewTransaction()
+      notValid = False
 
->>>>>>> a96fdd7f73400930dd8cbc762cb676395aa8c917
-
-
-<<<<<<< HEAD
-if len(sys.argv) < 2:
-  choice = input("Enter what function you would like to do\n(1) Create Account \n ")
-  if choice == "1":
-    datapayload = CreateAccount()
-    sqsMessageCommand = 'aws eventbridge put-events --queue-url https://sqs.us-east-1.amazonaws.com/072065628342/sqsToLambdaToEb --message-body \'' + datapayload+ '\''
-    os.system(sqsMessageCommand)
-    print('tried to run system command: ' + sqsMessageCommand)
-=======
->>>>>>> a96fdd7f73400930dd8cbc762cb676395aa8c917
-
-
-#Example
-# account = {
-#   "name" = name
-#   "address" = address
-# }
